ab08e03eb69dddb792e168c2164a2a7f821c1331
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-12-24 18:13:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ dovizjson = "https://api.agacinayetvar.ml/canli.json"
 @app.on_message(filters.command("start"))
 async def start(client, message):
     await client.send_message(message.chat.id, f"""
-"""**Merhaba İnsancık Ben Sana Güncel Doviz Kurunu Aktarıcam Komutları öğrenmek için /help komutunu Kullan.**""") 
+**Merhaba İnsancık Ben Sana Güncel Doviz Kurunu Aktarıcam Komutları öğrenmek için /help komutunu Kullan.**""") 
 
 # Degiskenlere atadigimiz veriyi Telegram'a yukluyoruz
 @app.on_message(filters.command("dolar"))
