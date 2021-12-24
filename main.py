983898a353b7f1c71229d0a6bc8744e8b9ee6d39
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-12-24 12:47:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def help(client, message):
     await client.send_message(message.chat.id, f"""
 **Merhaba İnsancık Ben Sana Güncel Doviz Kurunu Aktarıcam Komutları öğrenmek için /help komutunu Kullan eğer istersen botu grubuna ekleyerek kullanabilirsin tek yapman gereken aşağıdaki butona tıklamak.**""",
                     buttons=(
-            reply_markup=InlineKeyboardMarkup(
+                  reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Beni Grubuna Ekle", url="https://t.me/DovizBilgiBot?startgroup=a")]]),
                       reply_to_message_id=message.message_id)
 
