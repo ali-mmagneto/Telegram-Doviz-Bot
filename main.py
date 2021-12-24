4ae782abeb51e2ee997ce79f3e32c73f788413a4
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-12-24 12:45:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,16 +38,9 @@ async def help(client, message):
 **Merhaba İnsancık Ben Sana Güncel Doviz Kurunu Aktarıcam Komutları öğrenmek için /help komutunu Kullan eğer istersen botu grubuna ekleyerek kullanabilirsin tek yapman gereken aşağıdaki butona tıklamak.**""",
                     buttons=(
             reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "Beni Grubuna Ekle", url="https://t.me/DovizBilgiBot?startgroup=a"
-                        )
-                    ]
-                ]
-            ),
-            reply_to_message_id=message.message_id
-        )
+                [[InlineKeyboardButton(
+                     "Beni Grubuna Ekle", url="https://t.me/DovizBilgiBot?startgroup=a")]]),
+                      reply_to_message_id=message.message_id)
 
 # Degiskenlere atadigimiz veriyi Telegram'a yukluyoruz
 @app.on_message(filters.command("dolar"))
