4d08be9f37488a5e5c3881ab3d3190e910b61173
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-12-26 22:57:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,9 @@ import os
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
-if __name__ == "__main__" :
-    # create download directory, if not exist
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(
-        root="datalar"
-    )
     app = pyrogram.Client(
         "X-URL-Uploader",
-        bot_token=Config.TG_BOT_TOKEN,
+        bot_token=Config.BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=datalar
