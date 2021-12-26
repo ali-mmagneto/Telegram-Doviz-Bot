f336f2cb9e1288abdfc40a6092cba55598fab36e
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-12-26 23:45:48 +0300

    Update euro.py

--- plugins/euro.py
+++ plugins/euro.py
@@ -5,20 +5,6 @@ from pyrogram import Client
 from pyrogram import filters
 import os
 
-from os import getenv, environ
-
-bot_token = os.environ['BOT_TOKEN']
-api_id = int(os.environ['API_ID'])
-api_hash = os.environ['API_HASH']
-
-# Telegram sunucusuna bagliyoruz
-app = Client(
-    "Dovizbot",
-    bot_token=bot_token,
-    api_id=api_id,
-    api_hash=api_hash
-)
-
 # Json ile veri cekiyoruz
 dovizjson = "api.agacinayetvar.ml/doviz.json"
 
