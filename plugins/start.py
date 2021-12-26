35d9fffaaaac19bb6a16ae96301d217e0b0175e5
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-12-26 23:46:37 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,22 +6,6 @@ from pyrogram import Client
 from pyrogram import filters
 import os
 
-from os import getenv, environ
-
-bot_token = os.environ['BOT_TOKEN']
-api_id = int(os.environ['API_ID'])
-api_hash = os.environ['API_HASH']
-
-# Telegram sunucusuna bagliyoruz
-app = Client(
-    "Dovizbot",
-    bot_token=bot_token,
-    api_id=api_id,
-    api_hash=api_hash
-)
-
-dovizjson = "api.agacinayetvar.ml/doviz.json"
-
 @app.on_message(filters.command("start"))
 async def start(client, message):
     await client.send_message(message.chat.id, f"""
