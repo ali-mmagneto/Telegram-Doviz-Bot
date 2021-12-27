32092411b5d9e6c70a24abcfd721219cc93f8981
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-12-27 16:55:44 +0300

    Create main.py

---
+++ plugins/main.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import pyrogram
+from pyrogram import Client
+from pyrogram import filters
+import os
+
+from os import getenv, environ
+
+bot_token = os.environ['BOT_TOKEN']
+api_id = int(os.environ['API_ID'])
+api_hash = os.environ['API_HASH']
+
+# Telegram sunucusuna bagliyoruz
+app = Client(
+    "Dovizbot",
+    bot_token=bot_token,
+    api_id=api_id,
+    api_hash=api_hash,
+    plugins=plugins
+)
+
+app.run()
+ 
